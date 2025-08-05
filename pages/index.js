1970d60d5f889e67ab84560ce0b33c395613b351
Chaiwat Luerueang <chaiwat.luaruang@gmail.com>
2025-08-05 15:55:13 +0700

    test deploy dev

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ export default function Home() {
   return (
 <div className="bg-gray-900 text-white min-h-screen flex items-center justify-center">
   <h1 className="text-4xl font-bold">Tailwind v4 ใช้งานได้ ✅  test commit dev</h1>
+  <h1 className="text-4xl font-bold">Tailwind v4 ใช้งานได้ ✅  test commit dev</h1>
+  <h1 className="text-4xl font-bold">Tailwind v4 ใช้งานได้ ✅  test commit dev</h1>
 </div>
 
 
